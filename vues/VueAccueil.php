28142a63eaec9ceaac1c4634193ce7ed14f61cc9
delasalle-sio-boutin <delasalle.sio.boutin.k@gmail.com>
2016-11-04 22:33:55 +0100

    Premier commit

---
+++ vues/VueAccueil.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+	
+    <title>Association Inpact</title>
+	
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style.css" media="screen"/>
+</head>
+
+<body>
+	<div class="navbar-wrapper">
+	    <div class="container-fluid">
+	        <nav class="navbar navbar-fixed-top">
+	            <div class="container" >
+	                <div class="navbar-header">
+	                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+	                    <span class="sr-only">Toggle navigation</span>
+	                    <span class="icon-bar"></span>
+	                    <span class="icon-bar"></span>
+	                    <span class="icon-bar"></span>
+	                    </button>
+	                    <a href="VueAccueil.php" class="navbar-brand"><img src="../images/logo.png" style="width: 95px; height: 50px; margin-top: -5px;"></a>
+	                </div>
+	                <div id="navbar" class="navbar-collapse collapse" style="padding-top:6px;">
+	                    <ul class="nav navbar-nav">
+	                        <li><a href="VueAccueil.php" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" style="border-top: #000 solid 1px; margin-top: -14px; padding-top: 28px">Accueil</a></li>
+	                        <li class=" dropdown">
+	                            <a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Evenements <span class="caret"></span></a>
+	                            <ul class="dropdown-menu">
+	                                <li class=" dropdown">
+	                                    <a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">View Departments</a>
+	                                </li>
+	                                <li><a href="#">Add New</a></li>
+	                            </ul>
+	                        </li>
+	                        <li class=" dropdown"><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Liens utiles <span class="caret"></span></a>
+	                            <ul class="dropdown-menu">
+									<li><a href="#"><img id="liens" src="../images/dls.png">Lycée de la salle</a></li>
+									<li><a href="#"><img id="liens" src="../images/anciens_eleves.png">Anciens élèves</a></li>
+									<li><a href="#"><img id="liens" src="../images/dls_arena.png">DLS Arena</a></li>
+	                                <li><a href="#"><img id="liens" src="../images/facebook.png">Facebook</a></li>
+	                                <li><a href="#"><img id="liens" src="../images/twitter.png">Twitter</a></li>
+	                            </ul>
+	                        </li>
+							<li><a href="#">Contact</a></li>
+	                    </ul>
+	                    <ul class="nav navbar-nav pull-right">
+	                        <li><a href="#">Connecté en tant que</a></li>
+	                        <li><a href="#">Se déconnecter</a></li>
+	                    </ul>
+	                </div>
+	            </div>
+	        </nav>
+	    </div>
+	</div>
+
+ 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ vues/VueExemple.php
@@ -0,0 +1,12 @@
+<?php
+
+include ('VueAccueil.php');
+
+?>
+
+<div id="page">
+	Je m'appelle Killian, j'essaye ce site !
+	<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+	<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+	Et j'aime faire des tests.
+</div>
