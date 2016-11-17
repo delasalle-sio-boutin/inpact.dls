80f215ce78a73a3264cb0db47e5b9335ba44d116
Killian <delasalle.sio.boutin.k@gmail.com>
2016-11-17 17:01:16 +0100

    Modification du footer
    Ajout des logos des réseaux sociaux + Nom des menus

--- vues/Footer.php
+++ vues/Footer.php
@@ -1,38 +1,40 @@
 <!----------- Footer ------------>
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 <footer class="footer-bs fixed-bottom" id="footer" >
     <div class="row">
         <div class="col-xs-3 col-md-3 footer-logo animated fadeInLeft hidden-xs">
             <p><img src="images/logo.png"></p>
         </div>
-        <div class="col-xs-8 col-sm-6 footer-nav animated fadeInUp">
+        <div class="col-xs-4 col-sm-3 footer-nav animated fadeInUp">
             <h4>Menu</h4>
-           	<div class="col-xs-6">
+           	<div class="col-xs-12">
             	<ul class="pages">
-        	        <li><a href="#">Travel</a></li>
-                    <li><a href="#">Nature</a></li>
-                    <li><a href="#">Explores</a></li>
-                    <li><a href="#">Science</a></li>
-                    <li><a href="#">Advice</a></li>
+        	        <li><a href="#">Accueil</a></li>
+                    <li><a href="#">Evenements</a></li>
+                    <li><a href="#">Aide aux devoirs</a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
                 </ul>
             </div>
-          	<div class="col-xs-6">
+        </div>
+        <div class="col-xs-4 col-sm-3 footer-nav animated fadeInUp">
+            <h4>Informations</h4>
+          	<div class="col-xs-12">
                 <ul class="list">
-					<li><a href="#">About Us</a></li>
+					<li><a href="#">A propos</a></li>
                 	<li><a href="#">Contacts</a></li>
-                    <li><a href="#">Terms & Condition</a></li>
-                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">Termes & Condition</a></li>
+                    <li><a href="#">Politique de confidentialité</a></li>
                 </ul>
             </div>
         </div>
         <div class="col-xs-3 col-md-3 footer-social animated fadeInDown">
           	<h4>Follow Us</h4>
            	<ul>
-               	<li><a href="#">Facebook</a></li>
-               	<li><a href="#">Twitter</a></li>
-               	<li><a href="#">Instagram</a></li>
-               	<li><a href="#">RSS</a></li>
+               	<li><a href="https://www.facebook.com"><i class="fa fa-facebook fa-2x social-fb" id="social"></i></a>
+               	<a href="https://twitter.com/"><i class="fa fa-twitter fa-2x social-tw" id="social"></i></a>
+               	<a href="https://linkedin.com/"><i class="fa fa-linkedin fa-2x social-in" id="social"></i></a></li>
                	<!-- Ces "li" permettent de mettre le border left de "footer-social" jusqu'en bas de la page -->
-               	<li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li>
             </ul>
         </div> 	
     </div>
