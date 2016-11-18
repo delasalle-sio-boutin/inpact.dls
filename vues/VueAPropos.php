d479605b751d0960c37ee2bf6166e22e4a762ffa
delasalle-sio-gremy-f <delasalle.sio.gremy.florentin@gmail.com>
2016-11-18 16:17:37 +0100

    vueApropos en corus de modif

--- vues/VueAPropos.php
+++ vues/VueAPropos.php
@@ -1,28 +1,18 @@
 <header> <?php include ('Header.php'); ?> </header>
-
+<script src="https://use.fontawesome.com/3047271201.js"></script>
 <body>
 	<div id="page">
-		Qui sommes nous?
-L’ADPK (Association Des P‘tits Korrigans) est une jeune association qui a vu le jour au début de l’année 2011, en Bretagne, et qui s’est donné pour objectifs:
+		<h2>Qui sommes nous?</h2>
+		<br><br>
+		inpact est une association à but non-lucratif, sans passé économique, avec des projets déjà réalisé. 
+		
+		
 
-–  D’informer sur la maltraitance, sur les abandons, sur la stérilisation
-– De stériliser les chats errants sur des sites privés afin d’éviter la prolifération, les soigner, puis les relâcher sur le site d’origine ou sur un autre s’il y a danger
-– De récupérer des animaux abandonnés (chats, chiens et Nouveaux Animaux de Compagnie) et les faire adopter via un placement en Famille d’Accueil
-<br>
-Nous sommes toujours à la recherche:
+<br><br><br><br><br><br><br><br>
+page en cours de rédaction<i class="fa fa-spinner fa-spin fa-1x fa-fw"></i> <br>
+Longue vie et prospérité <br> <i class="fa fa-hand-spock-o" aria-hidden="false" fa-5x></i>
+<span class="sr-only">Loading...</span>
 
-– De familles d’accueil en Bretagne  (35, 56, 22, 29) ou dans les départements limitrophes (50, 53, 44, 14, 61, 49)
-– De parrains et de marraines (parrainer un animal, c’est contribuer à sa prise en charge vétérinaire, à sa nourriture,  à l’amélioration de son quotidien…)
-– D’adoptants, de préférence dans le grand ouest (Bretagne, Normandie Pays de Loire), mais possibilité d’adoption dans d’autres départements au cas par cas ( pré-visite, covoiturage assuré….)
-– De personnes ponctuellement disponibles pour du covoiturage
-– D’adhérents (télécharger le bulletin d’adhésion)
-– De dons financiers par paypal, virements, chèques (voir ici). L’ADPK est reconnue d’utilité publique, aussi pour 50 euros donnés, la dépense réelle n’est que de 17 euros car vous bénéficiez de 33 euros de crédit d’impôt.
-– De dons matériel (cages de convalescence, boites de transport chats, chiens, arbres a chat, jouets, croquettes, colliers, laisses, harnais chats…..)
-– De fournitures de bureau (ramettes de papier, enveloppes pré-timbrées ou non, timbres….)
-
-L’association fonctionne uniquement avec des familles d’accueil, nous ne disposons pas de refuges dédiés.
-
-Vous pouvez consulter les Statuts de l’association ainsi que le Règlement Intérieur.<i class="fa fa-shower" aria-hidden="true"></i><i class="fa fa-bath" aria-hidden="true"></i>
 	</div>
 </body>
 
