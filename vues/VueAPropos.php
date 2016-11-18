0ec205363112f39ec4907116c897b73541ec8f8f
delasalle-sio-gremy-f <delasalle.sio.gremy.florentin@gmail.com>
2016-11-18 15:18:09 +0100

    modif vue Apropos.php

--- vues/VueAPropos.php
+++ vues/VueAPropos.php
@@ -2,9 +2,27 @@
 
 <body>
 	<div id="page">
-		Phrase de début de page.
-		<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-		Phrase de fin de page.
+		Qui sommes nous?
+L’ADPK (Association Des P‘tits Korrigans) est une jeune association qui a vu le jour au début de l’année 2011, en Bretagne, et qui s’est donné pour objectifs:
+
+–  D’informer sur la maltraitance, sur les abandons, sur la stérilisation
+– De stériliser les chats errants sur des sites privés afin d’éviter la prolifération, les soigner, puis les relâcher sur le site d’origine ou sur un autre s’il y a danger
+– De récupérer des animaux abandonnés (chats, chiens et Nouveaux Animaux de Compagnie) et les faire adopter via un placement en Famille d’Accueil
+<br>
+Nous sommes toujours à la recherche:
+
+– De familles d’accueil en Bretagne  (35, 56, 22, 29) ou dans les départements limitrophes (50, 53, 44, 14, 61, 49)
+– De parrains et de marraines (parrainer un animal, c’est contribuer à sa prise en charge vétérinaire, à sa nourriture,  à l’amélioration de son quotidien…)
+– D’adoptants, de préférence dans le grand ouest (Bretagne, Normandie Pays de Loire), mais possibilité d’adoption dans d’autres départements au cas par cas ( pré-visite, covoiturage assuré….)
+– De personnes ponctuellement disponibles pour du covoiturage
+– D’adhérents (télécharger le bulletin d’adhésion)
+– De dons financiers par paypal, virements, chèques (voir ici). L’ADPK est reconnue d’utilité publique, aussi pour 50 euros donnés, la dépense réelle n’est que de 17 euros car vous bénéficiez de 33 euros de crédit d’impôt.
+– De dons matériel (cages de convalescence, boites de transport chats, chiens, arbres a chat, jouets, croquettes, colliers, laisses, harnais chats…..)
+– De fournitures de bureau (ramettes de papier, enveloppes pré-timbrées ou non, timbres….)
+
+L’association fonctionne uniquement avec des familles d’accueil, nous ne disposons pas de refuges dédiés.
+
+Vous pouvez consulter les Statuts de l’association ainsi que le Règlement Intérieur.
 	</div>
 </body>
 
