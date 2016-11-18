742673e5302ac68db3f188d644f51df1f07da9a9
delasalle-sio-gremy-f <delasalle.sio.gremy.florentin@gmail.com>
2016-11-18 16:46:34 +0100

    longue vie et prospérité

--- vues/VueAPropos.php
+++ vues/VueAPropos.php
@@ -4,13 +4,14 @@
 	<div id="page">
 		<h2>Qui sommes nous?</h2>
 		<br><br>
-		inpact est une association à but non-lucratif, sans passé économique, avec des projets déjà réalisé. 
+		<i class="fa fa-italic fa-3x fa-pull-left fa-border"aria-hidden="true"></i>
+		npact est une association à but non-lucratif, sans passé économique. Elle est connue pour ses événements organisés, tel que le repas des anciens et la dls_arena.
 		
 		
 
 <br><br><br><br><br><br><br><br>
 page en cours de rédaction<i class="fa fa-spinner fa-spin fa-1x fa-fw"></i> <br>
-Longue vie et prospérité <br> <i class="fa fa-hand-spock-o" aria-hidden="false" fa-5x></i>
+Longue vie et prospérité <br> <i class="fa fa-hand-spock-o fa-3x" aria-hidden="false"></i>
 <span class="sr-only">Loading...</span>
 
 	</div>
