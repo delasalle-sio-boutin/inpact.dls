2c66c8f10abf267d152a5d277e6883fffe916923
delasalle-sio-gremy-f <delasalle.sio.gremy.florentin@gmail.com>
2016-11-18 17:28:45 +0100

    g

--- vues/VueAPropos.php
+++ vues/VueAPropos.php
@@ -3,15 +3,18 @@
 <body>
 	<div id="page">
 		<h2>Qui sommes nous?</h2>
-		<br><br>
+		<br>
 		<i class="fa fa-italic fa-3x fa-pull-left fa-border"aria-hidden="true"></i>
-		npact est une association à but non-lucratif, sans passé économique. Elle est connue pour ses événements organisés, tel que le repas des anciens et la dls_arena.
-		
+		npact est une association à but non-lucratif appartenant a la filière SIO du lycée de la salle. Elle est connue pour ses événements organisés, tel que le repas des anciens et la dls arena.
+		Ce site a pour objectif d'améliorer la visibilité de l'association au sein de la section SIO du lycée. Mais aussi de faciliter l'organisation de projets futurs. 
+		<br><br>
+		<h2>Histoire du site</h2>
+		<br>
+		Le site est née d'un projet annuel de SIO2 en 2016. Une équipe de 5 développeurs et de deux administrateurs réseau ont permis la création du site.
 		
 
 <br><br><br><br><br><br><br><br>
 page en cours de rédaction<i class="fa fa-spinner fa-spin fa-1x fa-fw"></i> <br>
-Longue vie et prospérité <br> <i class="fa fa-hand-spock-o fa-3x" aria-hidden="false"></i>
 <span class="sr-only">Loading...</span>
 
 	</div>
