352bb30f8a89028534bd8f78de317caac4b89743
delasalle-sio-bienvenu-e <delasalle.sio.bienvenu.e@gmail.com>
2016-12-18 16:22:22 +0100

    Correction de fautes d'orthographe dans "A propos"

--- vues/VueAPropos.php
+++ vues/VueAPropos.php
@@ -19,19 +19,19 @@
 					L'objectif était de faire connaitre la formation, afin de proposer des services aux entreprises. 
 					A l'époque, il existait des avantages tels que des réductions à la FNAC, des réductions dans le café du coin et même dans des magasins de vêtements.
 					De plus, les étudiants organisaient des collectes de fond pour financer leurs projets.
-					Quant à l'origine du nom d'inpact, il est née de la volonté du professeur d'économie droit management, qui souhaitait un nom qui claque ! 
+					Quant à l'origine du nom d'inpact, il est né de la volonté du professeur d'économie droit management, qui souhaitait un nom qui claque ! 
 					D'où l'idée de "IMPACT" qui est devenue INPACT : association des <B>Informaticiens</B> <B>Pour</B> des <B>Actions</B> <B>Commerciales</B> et <B>Techniques</B>.
 					Mise à part les offres avantageuses et évènements organisés par l'association, inpact n'avait pas de site internet. Nous avons donc décidé d'en créer un.
 				<br><br>
 				<h2>Histoire du site</h2>
 				<br>
-					Le site est née d'un projet annuel de SIO2 en 2016. Une équipe de 5 développeurs et de deux administrateurs réseaux ont permis la création du site.
-					Nous trouvons que l'association a perdu de son importance ses dèrnières années. 
+					Le site est né d'un projet annuel de SIO2 en 2016. Une équipe de cinq développeurs et de deux administrateurs réseaux ont permis la création du site.
+					Nous trouvons que l'association a perdu de son importance ses dernières années. 
 					Nous avons donc pour objectif d'améliorer la visibilité de l'association au sein de la section SIO du lycée, Mais aussi de faciliter l'organisation de projets futurs à travers ce site.<br> 
 					Cette organisation est facilitée d'une part, par le listing des évènements. Et d'autre part, par les liens utiles d'inpact. 
-					L'aspect social est aussi très important, c'est pourquoi nous avons installé un flux Twitter, ainsi qu'un système d'aide aux devoirs qui peux s'avérer très utiles.
-					Pour finir, nous avons intégré scolinfo pour regrouper l'ensemble des sites utiles aux étudiants de sio.
-				<br><br>N'hésitez pas à nous contacter via notre formulaire pour de plus ample information.
+					L'aspect social est aussi très important, c'est pourquoi nous avons installé un flux Twitter, ainsi qu'un système d'aide aux devoirs qui peux s'avérer très utile.
+					Pour finir, nous avons intégré scolinfo pour regrouper l'ensemble des sites utiles aux étudiants de SIO.
+				<br><br>N'hésitez pas à nous contacter via notre formulaire pour de plus amples informations.
 				<br><br>
 				Team Inpact <i class="fa fa-user-circle" aria-hidden="true"></i>
 				</font>
