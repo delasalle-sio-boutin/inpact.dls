8772e30a87150a6a3835053907fa02b6b9db2378
delasalle-sio-audigou-s <audigou.s@I12-19.sio.local>
2017-03-03 16:31:28 +0100

    modification des aide devoirs

--- modele/AideDevoirs.test.php
+++ modele/AideDevoirs.test.php
@@ -1,2 +1,11 @@
 <?php
+// Projet : DLS - BTS Info - Site Inpact
+// fichier : modele/AideDevoirs.test.php
+// Rôle : test de la classe AideDevoirs
+// Création : 03/03/2017 par Sophie Audigou
+
+// inclusion de la classe Evenement
+include_once ('Evenement.class.php');
+// inclusion de la classe Outils
+include_once ('Outils.class.php');
 ?>
