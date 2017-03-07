316a043e866fdf149826357788e30c6d185d3a2e
delasalle-sio-gremy-f <delasalle.sio.gremy.florentin@gmail.com>
2017-03-07 17:20:12 +0100

    vue evenement: Adaptation de l'affichage d'un évènement en focntion de son id.

--- vues/VueEvenements.php
+++ vues/VueEvenements.php
@@ -9,27 +9,52 @@
 		<header> <?php include ('Header.php'); ?> </header>
 		
 		<div id="mainEvenement">
-		
-			<?php foreach ($lesEvenements as $unEvenement){ ?>
-			<a id="divCliquable" href="index.php?action=Evenements&id=<?php echo $unEvenement->getId() ?>">
-				<div class="ui-evenement-titre">
-					 <?php echo $unEvenement->getTitre() . " (du " . $unEvenement->getDateEvenement() . ")";?>
-					 	
-					 <br>
+			<?php if (isset($_GET['id'])) {
+				$idEvenement = $_GET['id'];?>
+				
+				<div class="divCliquable">
+					<div class="ui-evenement-titre">
+						 <?php echo $unEvenement->getTitre() . " (du " . $unEvenement->getDateEvenement() . ")";
+						 if (dateDiff($unEvenement->getDateEvenement()) == 0){
+							echo '<p style="float: right;"> Aujourd\'hui <br></p>';
+						}else{
+					 		echo '<p style="float: right;"> J-' . dateDiff($unEvenement->getDateEvenement()). '<br></p>';
+						} ?>	
+						 <br>
+					</div>
+					<div class="ui-evenement-evt" style="height: auto;">
+						 <?php echo $unEvenement->getContenu(); ?>
+						 <br>
+					</div>
+					<div class="ui-evenement-suite" style="height: 50px; padding: 5px;">
+						<a id="MsgRetour" href="index.php?action=Evenements" class="fa fa-arrow-left fa-2x"></a>
+					</div>
 				</div>
-				<div class="ui-evenement-evt">
-					 <?php echo $unEvenement->getContenu(); ?>
-					 <br>
-				</div>
-				<div class="ui-evenement-suite">
-					<?php if (dateDiff($unEvenement->getDateEvenement()) == 0){
+				
+				
+			<?php
+			} else {
+				foreach ($lesEvenements as $unEvenement){ ?>
+				<a id="divCliquable" href="index.php?action=Evenements&id=<?php echo $unEvenement->getId() ?>">
+					<div class="ui-evenement-titre">
+						 <?php echo $unEvenement->getTitre() . " (du " . $unEvenement->getDateEvenement() . ")";?>
+						 	
+						 <br>
+					</div>
+					<div class="ui-evenement-evt">
+						 <?php echo $unEvenement->getContenu(); ?>
+						 <br>
+					</div>
+					<div class="ui-evenement-suite">
+						<?php if (dateDiff($unEvenement->getDateEvenement()) == 0){
 							echo '<p style="float: right;"> Aujourd\'hui <br></p>';
 						}else{
 					 		echo '<p style="float: right;"> J-' . dateDiff($unEvenement->getDateEvenement()). '<br></p>';
 						} ?>
-				</div>
-			</a>
-			<?php } ?>
+					</div>
+				</a>
+				<?php } 
+			}?>
 		</div>
 		<footer class="footer-bs fixed-bottom" id="footer"> <?php include ('Footer.php'); ?> </footer>
 	</div>
